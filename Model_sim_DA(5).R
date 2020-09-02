4f2c130136e5eba4bb981a8dc59163d628f96a75
Yimeng Yin <marshallpku@gmail.com>
2020-09-01 20:12:12 -0400

    update results for DA with asset smoothing

--- Model_sim_DA(5).R
+++ Model_sim_DA(5).R
@@ -43,8 +43,8 @@ assign_parmsList(paramlist_,  envir = environment())
 # idxFull_DA
 
 # Override param values
-ncore <- 4
-nsim  <- 500
+# ncore <- 6
+# nsim  <- 500
 # smooth_method <- "method2"
 
 
